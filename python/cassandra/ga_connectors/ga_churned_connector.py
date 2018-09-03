e481b52931bffc02372e57fedaf1291a2856d476
abarbulescu <alexandru@appticles.com>
2018-09-03 13:38:42 +0300

    Refactor table names

--- python/cassandra/ga_connectors/ga_churned_connector.py
+++ python/cassandra/ga_connectors/ga_churned_connector.py
@@ -8,29 +8,29 @@ class GoogleAnalyticsChurn():
         self.analytics = GoogleAnalytics()
         self.store = CassandraPersistance()
         self.store.prepare_statements(
-            {'user_level': ['chu_users'], 'session_level': ['chu_sessions']})
+            {'user_level': ['users'], 'session_level': ['sessions']})
 
     # Get churned users
-    def store_chu_users(self):
+    def store_users(self):
         dimensions = ['dimension1', 'deviceCategory']
         metrics = ['sessions', 'sessionDuration', 'entrances',
                    'bounces', 'exits', 'pageValue', 'pageLoadTime', 'pageLoadSample']
 
-        return self.analytics.run_report_and_store('chu_users', dimensions, metrics, self.store)
+        return self.analytics.run_report_and_store('users', dimensions, metrics, self.store)
 
     # Get churned users with additional session data
-    def store_chu_sessions(self):
+    def store_sessions(self):
         dimensions = ['dimension1', 'dimension2',
                       'sessionCount', 'daysSinceLastSession']
         metrics = ['sessions', 'pageviews', 'uniquePageviews',
                    'screenViews', 'hits', 'timeOnPage']
 
-        return self.analytics.run_report_and_store('chu_sessions', dimensions, metrics, self.store)
+        return self.analytics.run_report_and_store('sessions', dimensions, metrics, self.store)
 
     def run(self):
         self.analytics.authenticate()
-        self.store_chu_users()
-        self.store_chu_sessions()
+        self.store_users()
+        self.store_sessions()
 
 
 if __name__ == '__main__':
